188355d66fbc73330eb693f166630596ab29ee23
techcmtai <ananya.namdev@completrix.com>
2025-09-17 16:42:30 +0530

    vbf

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,7 +20,7 @@ exports.protect = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
     // Fetch user from DB using decoded _id
-    const user = await User.findById(decoded._id).select('-password');
+    const user = await User.findById(decoded._id);
     if (!user) {
       return res.status(401).json({ message: 'User not found' });
     }
