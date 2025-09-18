6f4702e747445a84909fb9455fd45308b4e79233
techcmtai <ananya.namdev@completrix.com>
2025-09-18 11:01:26 +0530

    hd

--- middleware/uploadProducts.js
+++ middleware/uploadProducts.js
@@ -1,7 +1,7 @@
 // middlewares/uploadProfile.js
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../utils/cloudinary');
+const cloudinary = require('../config/cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary,
