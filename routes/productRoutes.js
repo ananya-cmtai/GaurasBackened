23dbd10ab95268b37807242ee37ba5711a919e5a
techcmtai <ananya.namdev@completrix.com>
2025-09-18 11:16:46 +0530

    hello

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { protect } = require('../middleware/auth');
 const uploadProducts =require("../middleware/uploadProducts");
-// Get all products
+
 router.get('/',protect, productController.getAllProducts);
 
-// Add new product (admin)
+
 router.post('/',protect,  uploadProducts.single('imageUrl'), productController.addProduct);
 
 
-router.put('/products/:productId',protect, uploadProducts.single('imageUrl'), productController.updateProduct); // The product ID is passed as a URL parameter
+router.put('/products/:productId',protect, uploadProducts.single('imageUrl'), productController.updateProduct); 
 
 module.exports = router;
