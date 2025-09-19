6313ffda0feb7ae2050819d72d8a64c66f9e2ddc
techcmtai <ananya.namdev@completrix.com>
2025-09-19 12:31:37 +0530

    bdfn

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,19 +4,21 @@ const userController = require('../controllers/userController');
 const walletController = require('../controllers/walletController');
 const { protect } = require('../middleware/auth');
 // Register user
+router.get('/profile', protect, userController.getProfile);  
+router.get('/:userId/wallet',protect, walletController.getWallet);
 router.post('/send-otp', userController.sendOtp);
 
 // Login user
 router.post('/verify-otp', userController.verifyOtp);
 
 // Get user wallet
-router.get('/:userId/wallet',protect, walletController.getWallet);
+
 
 // Add funds to wallet
 router.post('/:userId/wallet/add',protect, walletController.addFunds);
 
 router.put('/profile', protect, userController.updateProfile);
 
-router.get('/profile', protect, userController.getProfile);  
+
 
 module.exports = router;
