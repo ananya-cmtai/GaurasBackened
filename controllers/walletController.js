502ab743e8fe6661443e607e6349906c950d1c1a
techcmtai <ananya.namdev@completrix.com>
2025-09-20 11:29:35 +0530

    vd

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -59,8 +59,11 @@ exports.getWallet = async (req, res) => {
   try {
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: 'User not found' });
+       const sortedTransactions = user.wallet.transactions.sort(
+      (a, b) => new Date(b.date) - new Date(a.date)
+    );
 
-    res.status(200).json(user.wallet);
+    res.status(200).json(sortedTransactions);
   } catch (err) {
     res.status(500).json({
       message: 'Error fetching wallet',
