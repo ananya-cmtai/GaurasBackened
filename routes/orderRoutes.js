611d7c141eeda7a492cea1bac1d23fd3f4259393
techcmtai <ananya.namdev@completrix.com>
2025-09-22 16:19:56 +0530

    vds

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { protect } = require('../middleware/auth');
 // Place an order
-router.post('/',protect, orderController.placeOrder);
-
-// Get orders by user
+router.get('/by-delivery-boy', protect,orderController.getOrdersByDeliveryBoy);
 router.get('/user/:userId',protect, orderController.getOrdersByUser);
+router.post('/',protect, orderController.placeOrder);
+router.post('/assign-delivery-boy', orderController.assignDeliveryBoyToOrder);
+// Get orders by user
+
 
 router.put('/order/:orderId/status', orderController.updateOrderStatus);
        router.route('/razorpay/order').post(protect, orderController.createRazorpayOrderController);
 // routes/orderRoutes.js
-router.post('/assign-delivery-boy', orderController.assignDeliveryBoy);
 
 
-router.get('/by-delivery-boy', protect,orderController.getOrdersByDeliveryBoy);
+
+
 
 module.exports = router;
